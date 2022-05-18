4fb07208e5f3b84a93b8c3776c88ddecf92edd37
Dinh-Dung <vuongthanhdat2703@.gmail.com>
2022-05-18 15:44:44 +0700

    main_banner

--- client/src/components/Main/Header/Navigation.jsx
+++ client/src/components/Main/Header/Navigation.jsx
@@ -4,10 +4,10 @@ import './Navigation.css'
 import Background from '../../../images/anh_nen.jpg'
 
 
-export default function Header() {
+export default function MainHeader() {
   return (
     <div className='header'>
-      <div className="header_img " style={{ backgroundImage: `url(${Background})`, height: "800px" }}>
+      <div className="header_img ">
         <div className="header_nav">
           <nav className="navbar navbar-light  justify-content-between">
             <a className="navbar-brand" href="#undefine"> <Logo /></a>
@@ -26,22 +26,7 @@ export default function Header() {
           </nav>
         </div>
       </div>
-      <div className='Banner'>
-        <div className="container">
-          <h2>Unlimited movies, TV<br />shows, and more</h2>
-          <p>Watch anywhere. Cancel anytime.</p>
-          <div>
-            <a>Ready to watch? Enter your email to create or restart your membership</a>
-          </div>
-          <form>
-            <div className="form-group">
-              <label htmlFor="exampleInputEmail1">Email address</label>
-              <input type="email" className="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" />
-              <small id="emailHelp" className="form-text text-muted">We'll never share your email with anyone else.</small>
-            </div>
-          </form>
-        </div>
-      </div>
+
     </div>
   );
 
--- client/src/pages/index.jsx
+++ client/src/pages/index.jsx
@@ -1,19 +1,14 @@
 import React from 'react'
+import MainBanner from '../components/Main/banner/banner'
 // import Navigation from "../components/Home/Header/Navigation/Navigation.jsx";
 // import Banner from "../components/Home/Banner/Banner";
 import Navigation_main from "../components/Main/Header/Navigation"
 import "../styles/index.css"
+
 export default function IndexPage() {
   return (
-    <div className='App'>
-          <Navigation_main />
-        </div>
-    
-  //   <div className="App">
-  //   <header className="Header">
-  //       <Navigation />
-  //   </header>
-  //   <Banner/>
-  // </div>
+    <>
+      <MainBanner />
+    </>
   )
 }
