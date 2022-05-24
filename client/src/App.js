ebd79287b9b41dafea98be4903019e5e0f45d69b
Dinh-Dung <76037540+Dinh-Dung@users.noreply.github.com>
2022-05-24 21:52:52 +0700

    build userProfile

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import {Route, Routes } from "react-router-dom";
 import IndexPage from "./pages";
 import BrowsePage from "./pages/browse";
 import HomePage from "./pages/home";
+import Userprofile from "./pages/userprofile";
 import MoviePage from "./pages/movie";
 import Auth from "./pages/auth";
 
@@ -11,6 +12,7 @@ function App() {
     <Route path="/" element={<IndexPage />} />
     <Route path="/browse" element={<BrowsePage />} />
     <Route path="/homepage" element={<HomePage />}/>
+    <Route path="/userprofile" element={<Userprofile />}/>
     <Route path="/moviepage" element={<MoviePage />}/>
     <Route path="/auth" element={<Auth />}/>
   </Routes>
